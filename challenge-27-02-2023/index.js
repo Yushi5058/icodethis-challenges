1b48ca11b7bdfb6b77adddcc84fb6f8e78f89896
Yushi5058 <108335997+Yushi5058@users.noreply.github.com>
2023-02-27 11:23:28 +0100

    add listeners

--- challenge-27-02-2023/index.js
+++ challenge-27-02-2023/index.js
@@ -1,8 +1,16 @@
 const expandButton = document.querySelector('.expand-icon-js');
+const container = document.querySelector('.content-js');
+const closeButton = document.querySelector('.close-icon-js');
+
 
 expandButton.addEventListener('click', () => {
   const popUp = document.createElement('div');
   popUp.classList.add('popup');
-  popUp.innerText = '2450 words';
-  
+  popUp.innerText = '2450 words'
+  closeButton.style.display = 'block';
+  container.appendChild(popUp);
+
+  closeButton.addEventListener('click', () => {
+    popUp.style.display = 'none';
+  })
 })
