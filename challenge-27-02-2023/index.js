be144bf91edd92843bad9d1a6224e4fb389e59a9
Yushi5058 <108335997+Yushi5058@users.noreply.github.com>
2023-02-27 11:04:17 +0100

    create index.js

---
+++ challenge-27-02-2023/index.js
@@ -0,0 +1,8 @@
+const expandButton = document.querySelector('.expand-icon-js');
+
+expandButton.addEventListener('click', () => {
+  const popUp = document.createElement('div');
+  popUp.classList.add('popup');
+  popUp.innerText = '2450 words';
+  
+})
